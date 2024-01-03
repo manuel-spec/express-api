2a51cbbffe956e9e6628a917f84f071bbd56e8de
manuel-spec <amaniase084@gmail.com>
2024-01-03 09:49:56 +0300

    user create endpoint done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,24 @@
 const User = require('../models/User')
 const { validationResult, matchedData } = require('express-validator')
+const bcrypt = require('bcrypt')
 
 const store = async (request, response) => {
 
     const validation_result = validationResult(request)
     if (validation_result.isEmpty()) {
         const { username, password } = matchedData(request)
+        let hashedPassword = ''
+        bcrypt.hash(password, 10).
+            then(async (hash) => {
+                const user = await User.create({
+                    username: username,
+                    password: hash
+                })
+                return response.status(200).json({ "user": user })
 
-        const user = await User.create({ username, password })
-        return response.status(200).json({ "username": username, "password": password })
+            })
+
+
     } else {
         return response.status(400).json(validation_result)
     }
